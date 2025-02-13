1e17135e2a23c720ec01e8bf2f6793c65426718f
consult-with-josh <>
2025-02-13 09:18:27 +0100

    chore:updated copy script

--- scripts/scripts/copy.sh
+++ scripts/scripts/copy.sh
@@ -1,11 +1,10 @@
-
-#!/bin/bash
-
-list_***REMOVED***les_with_extension() {
+list_***REMOVED***les_with_two_extensions() {
     local dir_path=$1
-    local ***REMOVED***le_ext=$2
-    local exclude_dirs=(***REMOVED***${@:3}***REMOVED***)
+    local ext1=$2
+    local ext2=$3
+    local exclude_dirs=(***REMOVED***${@:4}***REMOVED***)
 
+    # Normalize excluded directories so we can match subpaths
     local exclude_dirs_set=()
     for dir in ***REMOVED***${exclude_dirs[@]}***REMOVED***; do
         exclude_dirs_set+=(***REMOVED***$(basename ***REMOVED***$dir***REMOVED*** | sed 's|/$||')***REMOVED***)
@@ -21,7 +20,7 @@ list_***REMOVED***les_with_extension() {
             ***REMOVED***
         done
 
-        if [ ***REMOVED***$exclude***REMOVED*** = false ] && [[ ***REMOVED***$***REMOVED***le***REMOVED*** == ****REMOVED***$***REMOVED***le_ext***REMOVED*** ]]; then
+        if [ ***REMOVED***$exclude***REMOVED*** = false ] && { [[ ***REMOVED***$***REMOVED***le***REMOVED*** == ****REMOVED***$ext1***REMOVED*** ]] || [[ ***REMOVED***$***REMOVED***le***REMOVED*** == ****REMOVED***$ext2***REMOVED*** ]]; }; then
             ***REMOVED***le_contents+=$'\n'***REMOVED***$(printf ***REMOVED***%s\n***REMOVED*** ***REMOVED***$***REMOVED***le***REMOVED*** | sed 's|^'***REMOVED***$dir_path***REMOVED***'/||')***REMOVED***  
             ***REMOVED***le_contents+=$'\n'***REMOVED***---------------------------------------------***REMOVED***
             ***REMOVED***le_contents+=$'\n'***REMOVED***$(cat ***REMOVED***$***REMOVED***le***REMOVED***)***REMOVED***
@@ -29,16 +28,19 @@ list_***REMOVED***les_with_extension() {
         ***REMOVED***
     done < <(***REMOVED***nd ***REMOVED***$dir_path***REMOVED*** -type f -print0)
 
-    
     echo ***REMOVED***$***REMOVED***le_contents***REMOVED*** | wl-copy
 }
 
-if [ ***REMOVED***$#***REMOVED*** -lt 2 ]; then
-    echo ***REMOVED***Usage: copy <directory_path> <***REMOVED***le_extension> [excluded_dir1] [excluded_dir2] ...***REMOVED***
+if [ ***REMOVED***$#***REMOVED*** -lt 3 ]; then
+    echo ***REMOVED***Usage: copy <directory_path> <***REMOVED***le_extension1> <***REMOVED***le_extension2> [excluded_dirs...]***REMOVED***
+    exit 1
 ***REMOVED***
-    dir_path=$1
-    ***REMOVED***le_ext=$2
-    shift 2
-    exclude_dirs=(***REMOVED***$@***REMOVED***)
-    list_***REMOVED***les_with_extension ***REMOVED***$dir_path***REMOVED*** ***REMOVED***$***REMOVED***le_ext***REMOVED*** ***REMOVED***${exclude_dirs[@]}***REMOVED***
-***REMOVED***
+
+dir_path=$1
+***REMOVED***le_ext1=$2
+***REMOVED***le_ext2=$3
+shift 3
+exclude_dirs=(***REMOVED***$@***REMOVED***)
+
+list_***REMOVED***les_with_two_extensions ***REMOVED***$dir_path***REMOVED*** ***REMOVED***$***REMOVED***le_ext1***REMOVED*** ***REMOVED***$***REMOVED***le_ext2***REMOVED*** ***REMOVED***${exclude_dirs[@]}***REMOVED***
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -6,7 +6,7 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-
+***REMOVED***
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
